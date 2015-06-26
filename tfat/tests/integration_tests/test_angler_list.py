d7054c1924db0d0b572c3642520559ffbaf55b79
Adam Cottrill <adam.cottrill@ontario.ca>
2015-06-26 11:58:59 -0400

    Added Create Person button to angler list and associated  tests
    If no anglers can be found matching the filter criteria - we need a way
    to add new anglers. A button to a (yet to be created form) was included
    with the message that no people could be found.  Wrote several
    integration tests to verify that angler filters are working as expected
    and that the button is available, and only available when no people are
    returned by the filters.

---
+++ tfat/tests/integration_tests/test_angler_list.py
@@ -0,0 +1,121 @@
+'''=============================================================
+c:/1work/Python/djcode/tfat/tfat/tests/integration_tests/test_angler_list.py
+Created: 26 Jun 2015 11:34:21
+
+
+DESCRIPTION:
+
+This test script ensurs that that the angler list renderes properly
+and that the option to add a new person is provided if a person
+matching the filter criteris is not found.
+
+A. Cottrill
+=============================================================
+
+'''
+
+
+import pytest
+from django.core.urlresolvers import reverse
+
+from tfat.tests.factories import *
+
+from datetime import datetime
+
+
+@pytest.fixture(scope='class')
+def db_setup():
+    """Create some users with easy to remember names.
+    """
+
+    angler1 = JoePublicFactory.create(first_name='Homer',
+                                      last_name='Simpson')
+
+    angler2 = JoePublicFactory.create(first_name='Montgomery',
+                                      last_name='Burns')
+
+    angler3 = JoePublicFactory.create(first_name='Barney',
+                                      last_name='Gumble')
+
+
+@pytest.mark.django_db
+def test_angler_list(client, db_setup):
+    """The default angler list should include a list of all anlgers in the
+    database.
+
+    """
+
+    url = reverse('angler_list')
+    response = client.get(url)
+    content = str(response.content)
+
+    assert 'Homer Simpson' in content
+    assert 'Montgomery Burns' in content
+    assert 'Barney Gumble' in content
+    assert 'George Costansa' not in content
+
+    #These should not appear in the response if people are returned
+    assert 'Sorry no people match that criteria' not in content
+    assert 'Add New Person' not in content
+
+
+@pytest.mark.django_db
+def test_angler_list_filter_first_name(client, db_setup):
+    """Verify that the filter works if we provide a partial first name
+    """
+
+    url = reverse('angler_list')
+    response = client.get(url + '?first_name=home')
+    content = str(response.content)
+
+    assert 'Homer Simpson' in content
+    assert 'Montgomery Burns' not in content
+    assert 'Barney Gumble' not in content
+    assert 'George Costansa' not in content
+
+    #These should not appear in the response if people are returned
+    assert 'Sorry no people match that criteria' not in content
+    assert 'Add New Person' not in content
+
+
+@pytest.mark.django_db
+def test_angler_list_filter_last_name(client, db_setup):
+    """Verify that the filter works if we provide a partial last name
+
+    """
+
+    url = reverse('angler_list')
+    response = client.get(url + '?last_name=impson')
+    content = str(response.content)
+
+    assert 'Homer Simpson' in content
+    assert 'Montgomery Burns' not in content
+    assert 'Barney Gumble' not in content
+    assert 'George Costansa' not in content
+
+    #These should not appear in the response if people are returned
+    assert 'Sorry no people match that criteria' not in content
+    assert 'Add New Person' not in content
+
+
+@pytest.mark.django_db
+def test_angler_not_in_angler_list(client, db_setup):
+    """If an angler who is not in the data base is queried, the response
+    should indicate that no people matching that criteria could be
+    found and provide an option to add a new person.
+
+    """
+
+    url = reverse('angler_list')
+    response = client.get(url + '?last_name=costanza')
+    content = str(response.content)
+
+    #what should not be there
+    assert 'Homer Simpson' not in content
+    assert 'Montgomery Burns' not in content
+    assert 'Barney Gumble' not in content
+    assert 'George Costansa' not in content
+
+    #what should be there:
+    assert 'Sorry no people match that criteria' in content
+    assert 'Add New Person' in content
